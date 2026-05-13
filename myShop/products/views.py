e66ce05d2dc8eb4fb75c964be10940f69a8c3c93
StrawberryProyo <shenaikam@gmail.com>
2026-05-13 11:59:15 +0800

    adds core pages, modal system (cart, wishlist, checkout, address), fixes venv issues, updates project structure, and adds UI styles

--- myShop/products/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Product
-
-# Create your views here.
-def index(request):
-    products = Product.objects.all().order_by('prodname')
-    return render(request, 'products/index.html', {'products': products})
